b3c506e99ec5da1828c0ffc51ab49ab3ac0d7411
penguin <slomogurl37@gmail.com>
2025-01-20 16:19:39 +0000

    Delete src/src directory

--- src/src/commands/echo/echo.rs
+++
@@ -1,9 +0,0 @@
-pub fn echo(command: &Vec<&str>) {
-    let length = command.len();
-    if length > 1 {
-        for i in 1..length {
-            print!("{} ",command[i]);
-        }
-    }
-    print!("\n");
-}
--- src/src/commands/neofetch/sysinfo/mod.rs
+++
@@ -1,5 +0,0 @@
-// src/commands/sysinfo/mod.rs
-pub mod memory;
-pub mod cpu;
-pub mod os;
-pub mod gpu;
